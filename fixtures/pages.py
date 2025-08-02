c29523f82c081c8205c8f4f8cc0d625d438c84c3
HelHitch <olena9002@gmail.com>
2025-08-02 17:27:35 +0400

    Практикуемся в реализации PageObject. Практикуемся в написании автотестов с использованием POM

--- fixtures/pages.py
+++ fixtures/pages.py
@@ -1,9 +1,11 @@
 import pytest
 from playwright.sync_api import Page
 
-from pages.DashboardPage import DashboardPage
-from pages.LoginPage import LoginPage
-from pages.RegistrationPage import RegistrationPage
+from pages.courses_list_page import CoursesListPage
+from pages.create_course_page import CreateCoursePage
+from pages.dashboard_page import DashboardPage
+from pages.login_page import LoginPage
+from pages.registration_page import RegistrationPage
 
 
 @pytest.fixture
@@ -17,3 +19,11 @@ def registration_page(page: Page):
 @pytest.fixture
 def dashboard_page(page: Page):
     return DashboardPage(page=page)
+
+@pytest.fixture
+def courses_list_page(chromium_page_with_state: Page) -> CoursesListPage:
+    return CoursesListPage(page=chromium_page_with_state)
+
+@pytest.fixture
+def create_course_page(chromium_page_with_state: Page) -> CreateCoursePage:
+    return CreateCoursePage(chromium_page_with_state)
--- tests/test_courses.py
+++ tests/test_courses.py
@@ -1,3 +1,6 @@
+import os
+from operator import index
+
 import pytest
 from playwright.sync_api import expect, Page
 
@@ -13,3 +16,59 @@ def test_empty_courses_list(chromium_page_with_state: Page):
 
     no_result_text = page.get_by_test_id("courses-list-empty-view-title-text")
     expect(no_result_text).to_have_text(expected="There is no results")
+
+@pytest.mark.courses
+@pytest.mark.regression
+def test_create_course(courses_list_page, create_course_page):
+    create_course_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses/create")
+
+    create_course_page.check_visible_create_course_title()
+    create_course_page.check_disabled_create_course_button()
+
+    create_course_page.check_visible_image_preview_empty_view()
+    create_course_page.check_visible_image_upload_view(is_image_uploaded=False)
+
+
+    create_course_page.check_visible_create_course_form(
+        title='',
+        description='',
+        estimated_time='',
+        min_score='0',
+        max_score='0'
+    )
+
+    create_course_page.check_visible_exercises_title()
+    create_course_page.check_visible_create_exercise_button()
+    create_course_page.check_visible_exercises_empty_view()
+
+    file_path = os.path.join('.', 'testdata', 'files', 'image.jpeg')
+    create_course_page.upload_preview_image(file=file_path)
+
+    create_course_page.check_visible_image_upload_view(is_image_uploaded=True)
+    course_data = {'title':"Playwright",
+                  'estimated_time':"2 weeks",
+                  'description':"Playwright",
+                  'max_score':"100",
+                  'min_score':"10"}
+
+    create_course_page.fill_create_course_form(
+        title=course_data['title'],
+        estimated_time=course_data['estimated_time'],
+        description=course_data['description'],
+        max_score=course_data['max_score'],
+        min_score=course_data['min_score']
+    )
+    create_course_page.click_create_course_button()
+
+    # Переход на страницу курсов
+    courses_list_page.check_visible_courses_title()
+    courses_list_page.check_visible_create_course_button()
+    courses_list_page.check_visible_course_card(
+        index=0,
+        title=course_data['title'],
+        estimated_time=course_data['estimated_time'],
+        max_score=course_data['max_score'],
+        min_score=course_data['min_score']
+    )
+
+
